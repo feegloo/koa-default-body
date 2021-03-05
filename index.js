a9752c20b5f0112a0e7b27f8a0f91e0a7d677622
Aleksander Figiel <olek.figiel@gmail.com>
2021-03-05 18:00:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,12 +6,8 @@ module.exports = routers => async (ctx, next) => {
 
   for (const r of Array.isArray(routers) ? routers: [routers]) {
     for (const stack of r.stack) {
-      // TODO: more bullet-proof replacing url
-      // use '/foo/:param1/:param2' of router and compare with actual ctx.url
-      const url = ['PATCH', 'DELETE'].includes(ctx.method) ? ctx.url.replace(/\/[^\/]*$/, '') : ctx.url
-
       if (
-        stack.path.includes(url) &&
+        stack.regexp.test(ctx.path) &&
         stack.methods.includes(ctx.method) &&
         ctx.status === 404 &&
         ctx.body === undefined
